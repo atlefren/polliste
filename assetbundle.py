35965643acb599fd8f870d0a786ecdfcdabd6fc2
Atle Frenvik Sveen <atle.sveen@gmail.com>
2013-11-24 23:26:38 +0100

    form for creating a pol

--- assetbundle.py
+++ assetbundle.py
@@ -1,7 +1,13 @@
-from flask.ext.assets import  Bundle
+from flask.ext.assets import Bundle
 
 css = Bundle(
     'css/style.css',
     filters=['cssmin', 'cssrewrite'],
     output='gen/css/style.css'
+)
+
+adminpage_js = Bundle(
+    'js/src/pol.js',
+    filters=['jsmin'],
+    output='gen/js/adminpage.js'
 )
---
+++ polliste/static/js/src/pol.js
@@ -0,0 +1,41 @@
+var Polliste = window.Polliste || {};
+
+(function (ns, undefined) {
+    "use strict";
+
+    var Pol = Backbone.Model.extend({
+        url: "/api/v1/pol/"
+    });
+
+    ns.PolCreateForm = Backbone.View.extend({
+
+        tagName: "form",
+        className: "form-horizontal",
+
+        events: {
+            "submit": "createPol"
+        },
+
+        template: $("#pol_create_template").html(),
+
+        initialize: function () {
+            _.bindAll(this, "createPol");
+        },
+
+        render: function () {
+            this.$el.append(_.template(this.template));
+            return this;
+        },
+
+        createPol: function () {
+            var pol = new Pol({"name": this.$("#inputName").val()});
+            pol.save({}, {"success": this.saved});
+            return false;
+        },
+
+        saved: function () {
+            location.reload();
+        }
+    });
+
+}(Polliste));
